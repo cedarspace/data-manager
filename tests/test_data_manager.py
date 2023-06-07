b76083e908102d1e2f1eb5c7dbeda6978e34c17d
Dante <dacerron@gmail.com>
2023-06-07 12:51:55 -0700

    fixed import, now there are errors in cli.py

--- tests/test_data_manager.py
+++ tests/test_data_manager.py
@@ -1,16 +1,17 @@
 # tests/test_rptodo.py
 import sys
     # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '/Users/cedarspace/Desktop/Soghmon_files/DataManager_library/datamanager_project/data_manager')
+sys.path.insert(1, '../data_manager')
 
 import data_manager       
 
 
 import check_output
 from typer.testing import CliRunner
+import cli
 
-cli = data_manager.cli
-import data_manager.cli
+#cli = data_manager.cli
+#import data_manager.cli
 
 runner = CliRunner()
 
