c49c882853098569ae7c4690d63d06c9ddb7eda1
Saughmon Boujkian <soghmon5@gmail.com>
2023-06-17 21:40:17 -0700

    Update __init.py

--- tests/__init.py
+++ tests/__init.py
@@ -1,12 +1,1 @@
 
-list_special = ["@", "!"]
-def loda(s):
-    if s[0] in list_special:
-            print(True)
-    else: 
-        if len(s) != 1:  
-            loda(s[1:])
-        else: 
-            print(False)
-
-loda("mathematics!")
